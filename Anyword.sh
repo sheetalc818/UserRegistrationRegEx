6b4e6263cb4829f8f9c6132731ef1843c1117908
sheetalc818 <sheetalc818@gmail.com>
2020-02-18 13:08:17 +0530

    Added : User need to enter valid Firstname with Min 3 length and Start with capital letter

--- Anyword.sh
+++ Anyword.sh
@@ -1,3 +1,28 @@
 #!/bin/bash -x
 
 echo "User Registration RegEx!!"
+
+echo "Enter a word to check for the regex:"
+read word
+
+#Checking for the firstname Starting with capital letter and having min length 3
+function firstNameRegEx()
+{
+	firstname=$1
+	regEx=^[A-Z]
+	size=${#firstname}
+	if [ $size -gt 2 ]
+	then
+		if [[ $firstname =~ $regEx ]]
+		then
+      	echo "Valid word!!"
+		else
+      	echo "Please enter Word start with Capital letter!!"
+		fi
+	else
+			echo "Enter the FirstName having length greater than 2!!"
+	fi
+}
+
+#Calling function
+firstNameRegEx $word
