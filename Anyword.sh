78ba1f3b0fc357ea718df9a5908ab1d07a7d2284
sheetalc818 <sheetalc818@gmail.com>
2020-02-19 17:49:51 +0530

    Added : RegEx for validating lastname which start with Capital letter and min length 3

--- Anyword.sh
+++ Anyword.sh
@@ -1,28 +1,49 @@
 #!/bin/bash -x
 
 echo "User Registration RegEx!!"
-
-echo "Enter a word to check for the regex:"
-read word
-
+ 
 #Checking for the firstname Starting with capital letter and having min length 3
 function firstNameRegEx()
 {
-	firstname=$1
+	echo "Enter firstname to check for the regex:"
+	read firstname
+
 	regEx=^[A-Z]
 	size=${#firstname}
 	if [ $size -gt 2 ]
 	then
 		if [[ $firstname =~ $regEx ]]
 		then
-      	echo "Valid word!!"
+      	echo "Valid firstname!!"
 		else
-      	echo "Please enter Word start with Capital letter!!"
+      	echo "Please enter firstname start with Capital letter!!"
 		fi
 	else
-			echo "Enter the FirstName having length greater than 2!!"
+		echo "Enter the FirstName having length greater than 2!!"
+	fi
+}
+
+#Checking for the lastname Starting with capital letter and having min length 3
+function lastNameRegEx()
+{
+	echo "Enter lastname to check for the regex:"
+	read lastname
+
+	regEx=^[A-Z]
+	size=${#lastname}
+	if [ $size -gt 2 ]
+	then
+		if [[ $lastname =~ $regEx ]]
+		then
+			echo "Valid lastname!!"
+		else
+			echo "Please enter lastname start with Capital letter!!"
+		fi
+	else
+		echo "Enter the lastname having length greater than 2!!"
 	fi
 }
 
 #Calling function
-firstNameRegEx $word
+firstNameRegEx
+lastNameRegEx
