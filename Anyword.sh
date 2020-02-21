96821375b608ba8ff812302007d82b44c661e621
sheetalc818 <sheetalc818@gmail.com>
2020-02-21 16:54:25 +0530

    Refactor : RegEx written for Firstname and Lastname verification Added : RegEx for Email verification

--- Anyword.sh
+++ Anyword.sh
@@ -8,18 +8,12 @@ function firstNameRegEx()
 	echo "Enter firstname to check for the regex:"
 	read firstname
 
-	regEx=^[A-Z]
-	size=${#firstname}
-	if [ $size -gt 2 ]
+	regEx="[A-Z][a-zA-Z]{2,}"
+	if [[ $firstname =~ $regEx ]]
 	then
-		if [[ $firstname =~ $regEx ]]
-		then
-      	echo "Valid firstname!!"
-		else
-      	echo "Please enter firstname start with Capital letter!!"
-		fi
+      echo "Valid firstname!!"
 	else
-		echo "Enter the FirstName having length greater than 2!!"
+      echo "Please enter firstname start with Capital letter and having min length 3!!"
 	fi
 }
 
@@ -29,21 +23,33 @@ function lastNameRegEx()
 	echo "Enter lastname to check for the regex:"
 	read lastname
 
-	regEx=^[A-Z]
-	size=${#lastname}
-	if [ $size -gt 2 ]
+	regEx="[A-Z][a-zA-Z]{2,}"
+
+	if [[ $lastname =~ $regEx ]]
 	then
-		if [[ $lastname =~ $regEx ]]
-		then
-			echo "Valid lastname!!"
-		else
-			echo "Please enter lastname start with Capital letter!!"
-		fi
+		echo "Valid lastname!!"
 	else
-		echo "Enter the lastname having length greater than 2!!"
+		echo "Please enter lastname start with Capital letter and having min length 3!!"
+	fi
+}
+
+#Checking for the E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl
+#& co) and 2 optional (xyz & in) with precise @ and . positions
+function emailRegEx()
+{
+	echo "Enter Email to check for the regex:"
+	read email
+
+	regEx="[0-9a-zA-Z]+[._+-]{0,1}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-z]{2}){0,1}$"
+	if [[ $email =~ $regEx ]]
+	then
+		echo "Valid Email!!"
+	else
+		echo "Please enter valid email id!!"
 	fi
 }
 
 #Calling function
 firstNameRegEx
 lastNameRegEx
+emailRegEx
